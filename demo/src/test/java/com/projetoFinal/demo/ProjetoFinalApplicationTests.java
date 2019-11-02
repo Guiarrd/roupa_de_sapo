02161da30539e76567e1acbb41674ec8b695e7c6
Eduardo <eduardo_mangrich@hotmail.com>
2019-11-02 17:42:20 -0300

    Spring Boot Initializer

---
+++ demo/src/test/java/com/projetoFinal/demo/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoFinal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
