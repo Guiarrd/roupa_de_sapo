2563cd62269a5801d491be653da73f2df52cb550
Guilherme Arruda <guilherme_arruda@live.com>
2019-11-12 17:41:12 -0300

    Correção das rotas do log e criação de outras que faltavam

--- centralErros/src/main/java/com/projetoFinal/centralErros/repository/LogRepository.java
+++ centralErros/src/main/java/com/projetoFinal/centralErros/repository/LogRepository.java
@@ -15,7 +15,7 @@ public interface LogRepository extends JpaRepository<Log, Long> {
 
     //traz todos os Logs baseado na opção que o usuário escolher nas caixas de seleção de environment e level
 
-    @Query(value = "Select l from Log l where l.environmentEnum=?1 order by ?2")
+    @Query(value = "Select l from Log l where l.environmentEnum=?1 order by CASE WHEN (l.levelEnum = ?1) THEN 1 END DESC")
     List<Log> findAllByEnvironmentOrderLevel(EnvironmentEnum env, LevelEnum levelEnum);
 
     @Query(value = "Select l from Log l where l.environmentEnum=?1")
@@ -26,12 +26,10 @@ public interface LogRepository extends JpaRepository<Log, Long> {
     List<Log> findAllBySearch(String search);
 
     @Query(value = "Select l from Log l where l.archived=true")
-    List<Log> findAllByArchieved();
+    List<Log> findAllByArchived();
 
-
-    @Query(value = "Select l from Log l order by ?2")
+    @Query(value = "Select l from Log l order by CASE WHEN (l.levelEnum = ?1) THEN 1 END DESC")
     List<Log> findAllOrderByLevel(LevelEnum levelEnum);
-
 
     Optional<Log> findById(Long id);
 
