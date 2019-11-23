7fc279a0e6233cbec5d2810593efd9de33c2f183
Eduardo <eduardo_mangrich@hotmail.com>
2019-11-23 08:00:22 -0300

    Retorno do response entity foi especificado:

--- centralErros/src/main/java/com/projetoFinal/centralErros/controller/UserController.java
+++ centralErros/src/main/java/com/projetoFinal/centralErros/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -19,36 +20,36 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping // acessar /user via POST para cadastrar um usuário
-    public ResponseEntity<?> saveUser(@Valid @RequestBody User user){
+    public ResponseEntity<HttpStatus> saveUser(@Valid @RequestBody User user){
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{userId}") // acessar /user/id via PUT para atualizar um usuário
-    public ResponseEntity<?> updateUser(@Valid @RequestBody User user, @PathVariable Long userId) {
+    public ResponseEntity<HttpStatus> updateUser(@Valid @RequestBody User user, @PathVariable Long userId) {
         user.setId(userId);
         userService.saveUser(user);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping // acessar /user via GET para listar todos os usuários
-    public ResponseEntity<?> findAllUsers() {
+    public ResponseEntity<List<User>> findAllUsers() {
         return new ResponseEntity <>(UserMapper.toListUser(userService.findAllUsers()), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}") // acessar /user/id via GET para listar um usuário
-    public ResponseEntity<?> findUserById(@PathVariable Long userId){
+    public ResponseEntity<User> findUserById(@PathVariable Long userId){
         return new ResponseEntity<>(UserMapper.toUser(userService.findUserById(userId)), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{userId}") // acessar /delete/id via DELETE para deletar um usuário
-    public ResponseEntity<?> deleteUser(@PathVariable Long userId) {
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long userId) {
         userService.deleteUser(userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(method = RequestMethod.GET, params = "email") // acessar /user?email=EMAIL via GET para listar um usuário pelo seu e-mail
-    public ResponseEntity<?> findByEmail(@RequestParam(value="email", required = false) String email) {
+    public ResponseEntity<User> findByEmail(@RequestParam(value="email", required = false) String email) {
         return new ResponseEntity<>(UserMapper.toUser(userService.findByEmail(email)), HttpStatus.OK);
     }
 }
