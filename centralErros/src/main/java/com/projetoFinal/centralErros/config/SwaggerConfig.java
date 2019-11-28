dead89a55a7b8b0c74085e4d0b8e42b90294e7ee
Eduardo <eduardo_mangrich@hotmail.com>
2019-11-28 19:24:08 -0300

    Swagger adicionado

---
+++ centralErros/src/main/java/com/projetoFinal/centralErros/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.projetoFinal.centralErros.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static org.reflections.util.ConfigurationBuilder.build;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.projetoFinal.centralErros"))
+                .paths(regex("/.*"))
+                .build();
+
+    }
+}
